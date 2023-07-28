6a894b2c4c72e59c9ad748ad6f5e73bf1c9e8ef8
Azizo_pu01 <Azizo_pu01@STHQ0127-03>
2023-07-28 18:42:40 +0400

    end task

--- Network ImageTask Client/ViewModels/MainViewModel.cs
+++ Network ImageTask Client/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Network_ImageTask_Client.Commands;
+﻿using Microsoft.Win32;
+using Network_ImageTask_Client.Commands;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -9,6 +10,8 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Markup;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace Network_ImageTask_Client.ViewModels
@@ -18,9 +21,9 @@ namespace Network_ImageTask_Client.ViewModels
         public RelayCommand SendButtonCommand { get; set; }
         public RelayCommand AddImageButtonCommand { get; set; }
 
-        private string image;
+        private BitmapImage image;
 
-        public string Image
+        public BitmapImage Image
         {
             get { return image; }
             set { image = value; OnPropertyChanged(); }
@@ -28,55 +31,32 @@ namespace Network_ImageTask_Client.ViewModels
 
         public MainViewModel()
         {
+
             AddImageButtonCommand = new RelayCommand((_) =>
             {
-                Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
-                dlg.FileName = "Image"; // Default file name
-                dlg.DefaultExt = ".png"; // Default file extension
-                dlg.Filter = "Image Files (.jpg)|*.jpg;*.jpeg;*.png;*.gif;*.tif;"; // Filter files by extension
-
-                // Show open file dialog box
-                Nullable<bool> result = dlg.ShowDialog();
-
-                // Process open file dialog box results
-                if (result == true)
-                {
-                    // Open document
-                    string filename = dlg.FileName;
-                    //var name = filename.Split('\\');
-                    //for (int i = 0; i < a.Length; i++)
-                    //{
-
-                    //}
-                    //BitmapImage filefoto = new BitmapImage();
-                    //filefoto.UriSource = new Uri(filename);
-
-                    //var buffer = new byte[34000];
-                    Image = filename;
-                }
+                File_send(Image);
             });
 
             SendButtonCommand = new RelayCommand((_) =>
             {
+                var ipAdress = IPAddress.Parse("10.2.27.3");
+                var port = 27034;
+
                 Task.Run(() =>
                 {
-                    var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-                    var ipAdress = IPAddress.Parse("10.2.29.13");
-                    var port = 27014;
-
                     var ep = new IPEndPoint(ipAdress, port);
 
                     try
                     {
+                        var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                         socket.Connect(ep);
+
 
                         if (socket.Connected)
                         {
                             var sendImage = Image;
-                            var bytes = Encoding.ASCII.GetBytes(sendImage);
+                            var bytes = GetJPGFromImageControl(Image);
                             socket.Send(bytes);
-
                         }
                     }
                     catch (Exception ex)
@@ -86,28 +66,28 @@ namespace Network_ImageTask_Client.ViewModels
                 });
             });
         }
-        public string GetImagePath(byte[] buffer)
+
+        public void File_send(object parametr)
         {
-            ImageConverter ic = new ImageConverter();
-            var data = ic.ConvertFrom(buffer);
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "Image";
+            dlg.DefaultExt = ".png";
 
-            Image img = data as Image;
-            if (img != null)
+            if (dlg.ShowDialog() == true)
             {
-                Bitmap bitmap1 = new Bitmap(img);
-
-                var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Images2");
-                Directory.CreateDirectory(path);
-                var strGuid = Guid.NewGuid().ToString();
-                bitmap1.Save($@"{path}\image{strGuid}.png");
-                var imagepath = $@"{path}\image{strGuid}.png";
-                return imagepath;
+                Image = new BitmapImage(new Uri(dlg.FileName));
+                ImageBrush brush = new ImageBrush(Image);
+                //IsOkay = true;
             }
-            else
-            {
-                return String.Empty;
-            }
+        }
 
+        public byte[] GetJPGFromImageControl(BitmapImage imageC)
+        {
+            MemoryStream memStream = new MemoryStream();
+            JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(imageC));
+            encoder.Save(memStream);
+            return memStream.ToArray();
         }
     }
 
